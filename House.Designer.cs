6a9d3bd14381bb429627c778cdaacb839a2e7169
Shan Ahmed <21813031@bucks.ac.uk>
2020-06-12 05:06:22 +0100

    Add project files.

---
+++ House.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace drawHouse
+{
+    partial class House
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // House
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 450);
+            this.Name = "House";
+            this.Text = "Draw a House";
+            this.Load += new System.EventHandler(this.House_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.House_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ House.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drawHouse
+{
+    public partial class House : Form
+    {
+        /// <summary>
+        /// Independent Study Task 6.9
+        /// Author: Shan Ahmed 21813031
+        /// This application draws a simple house.
+        /// </summary>
+        /// 
+
+        public House()
+        {
+            InitializeComponent();
+        }
+
+        private void House_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Below creates the necessary shapes and paints them in 
+        /// to draw the house
+        /// </summary>
+
+        private void House_Paint(object sender, PaintEventArgs e)
+        {
+            Pen myPen = new Pen(Color.Black, 5);
+            Graphics g = e.Graphics;
+            int x = 100, y = 100, size = 200;
+            g.DrawRectangle(myPen, x, y, size, size);
+            g.FillRectangle(Brushes.Brown, x, y, size, size);
+
+            Point[] roof = new Point[]                               
+            {
+                new Point(100,100),
+                new Point(200,0),
+                new Point(300,100),
+            
+            };
+            g.DrawPolygon(myPen, roof);                              //Draws Roof
+            g.FillPolygon(Brushes.Black,roof);
+
+            g.DrawRectangle(myPen, 180, 230, 40, 70);                //Draws Door
+            g.FillRectangle(Brushes.SandyBrown, 180, 230, 40, 70);
+
+            g.DrawRectangle(myPen, 130, 130, 50, 50);                //Draws window
+            g.FillRectangle(Brushes.White, 130, 130, 50, 50);
+
+            g.DrawRectangle(myPen, 220, 130, 50, 50);                //Draws window
+            g.FillRectangle(Brushes.White, 220, 130, 50, 50);
+
+
+
+        }
+    }
+}
